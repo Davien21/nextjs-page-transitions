8d26dc243b44511b3f9284bf6085c1d36d60bd05
davien21 <chidiebereekennia@gmail.com>
2021-05-25 12:08:40 +0100

    Add fadeIn and fadeOut animation

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,23 @@
 import Link from "next/link";
 import fetch from "isomorphic-unfetch";
-
-const Index = props => (
-  <div>
-    <div className='container center'>
-      <div className='title'>
+import { motion } from "framer-motion";
+const Index = (props) => (
+  <motion.div exit={{ opacity: 0 }}>
+    <div className="container center">
+      <div className="title">
         <h1>Select a protein</h1>
       </div>
-      <div className='product-row'>
-        {props.products.map(product => (
+      <div className="product-row">
+        {props.products.map((product) => (
           <Link
             key={product.id}
-            href='/products/[id]'
-            as={`/products/${product.id}`}>
-            <div className='card'>
-              <span className='category'>Protein</span>
+            href="/products/[id]"
+            as={`/products/${product.id}`}
+          >
+            <div className="card">
+              <span className="category">Protein</span>
               <img key={product.image} src={product.image} width={250} />
-              <div className='product-info'>
+              <div className="product-info">
                 <h4>{product.name}</h4>
                 <span>{product.price}</span>
               </div>
@@ -25,16 +26,20 @@ const Index = props => (
         ))}
       </div>
     </div>
-  </div>
+  </motion.div>
 );
 
-Index.getInitialProps = async function() {
+Index.getInitialProps = async function () {
   const res = await fetch(
     "http://my-json-server.typicode.com/wrongakram/demo/products"
   );
+  let imageUrl =
+    "https://cdn.shopify.com/s/files/1/2060/6331/products/WheyCA.png?v=1596573521";
+
   const data = await res.json();
+  data.map((thing) => (thing.image = imageUrl));
   return {
-    products: data
+    products: data,
   };
 };
 
--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,54 +1,60 @@
 import fetch from "isomorphic-unfetch";
 import Link from "next/link";
-
-const Product = props => (
-  <div>
-    <div className='fullscreen'>
-      <div className='product'>
-        <div className='img'>
+import { motion } from "framer-motion";
+const Product = (props) => (
+  <motion.div
+    exit={{ opacity: 0 }}
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+  >
+    <div className="fullscreen">
+      <div className="product">
+        <div className="img">
           <img key={props.product.image} src={props.product.image} />
         </div>
-        <div className='product-details'>
-          <div className='inner'>
-            <Link href='/'>
+        <div className="product-details">
+          <div className="inner">
+            <Link href="/">
               <div>
-                <a className='go-back'>Back to products</a>
+                <a className="go-back">Back to products</a>
               </div>
             </Link>
             <div>
-              <span className='category'>Protein</span>
+              <span className="category">Protein</span>
             </div>
             <h1>{props.product.name}</h1>
             <p>{props.product.details}</p>
-            <div className='additonals'>
+            <div className="additonals">
               <span>Soy Free</span>
               <span>Gluten Free</span>
             </div>
-            <div className='qty-price'>
-              <div className='qty'>
-                <div className='minus'>-</div>
-                <div className='amount'>1</div>
-                <div className='add'>+</div>
+            <div className="qty-price">
+              <div className="qty">
+                <div className="minus">-</div>
+                <div className="amount">1</div>
+                <div className="add">+</div>
               </div>
-              <span className='price'>{props.product.price}</span>
+              <span className="price">{props.product.price}</span>
             </div>
-            <div className='btn-row'>
-              <button className='add-to-cart'> Add to cart</button>
-              <button className='subscribe'> Subscribe</button>
+            <div className="btn-row">
+              <button className="add-to-cart"> Add to cart</button>
+              <button className="subscribe"> Subscribe</button>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  </motion.div>
 );
 
-Product.getInitialProps = async function(context) {
+Product.getInitialProps = async function (context) {
   const { id } = context.query;
   const res = await fetch(
     `http://my-json-server.typicode.com/wrongakram/demo/products/${id}`
   );
   const product = await res.json();
+  product.image =
+    "https://cdn.shopify.com/s/files/1/2060/6331/products/WheyCA.png?v=1596573521";
   return { product };
 };
 
